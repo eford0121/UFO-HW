20afc95d6ea809482b78bc86b802ac0c1b5df545
Liz Lemon <eford0121@gmail.com>
2019-01-07 18:38:38 -0600

    Adding notes

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ var tableData = data;
 
 var tbody = d3.select("tbody"); 
 
-// use arrow functions
+// use arrow functions to import data
 
 tableData.forEach((ufoSightings) => {
     var row = tbody.append("tr");
@@ -14,11 +14,15 @@ tableData.forEach((ufoSightings) => {
         cell.text(value);
     });
 });
+
+// filtering data based in input
+
+
 var sightings = data;
 var filterButton =d3.select("#filter-btn");
-
+// defining filter function
 filterButton.on("click", function() {
-    
+    //prevent refresh
     d3.event.preventDefault();
     d3.selectAll("td").remove()
 
@@ -28,7 +32,7 @@ filterButton.on("click", function() {
 
     console.log(inputValue);
     console.log(inputElement);
-
+     //making sightings equal to date of input
     var filteredData = sightings.filter(sighting => sighting.datetime === inputValue);
     
     console.log("Here are your search results:", filteredData);
@@ -36,6 +40,7 @@ filterButton.on("click", function() {
     var tbody = d3.select("tbody");
         //console.log(data);
 
+    //append page to reflect the new, filtered sightings
     filteredData.forEach((filteredSightings) => {
         var row = tbody.append("tr");
         Object.entries(filteredSightings).forEach(([key, value]) =>{
